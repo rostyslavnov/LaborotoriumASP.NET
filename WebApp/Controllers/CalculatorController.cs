ca116931e94216bf705d9d31669d7ef0f3d1517b
rostyslav.novytskyi <rostyslav.novytskyi@microsfot.wsei.edu.pl>
2024-10-15 17:19:09 +0200

    result

---
+++ WebApp/Controllers/CalculatorController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApp.Controllers;
+
+public class CalculatorController : Controller
+{
+    // GET
+    public IActionResult Result()
+    {
+        return Result();
+    }
+}
