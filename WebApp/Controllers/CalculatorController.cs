5b03e3115ba2b864d5a3d24bb800ab195dc70e3d
rostyslav.novytskyi <rostyslav.novytskyi@microsfot.wsei.edu.pl>
2024-10-15 17:40:04 +0200

    index

--- WebApp/Controllers/CalculatorController.cs
+++ WebApp/Controllers/CalculatorController.cs
@@ -4,9 +4,70 @@ namespace WebApp.Controllers;
 
 public class CalculatorController : Controller
 {
-    // GET
-    public IActionResult Result()
+    public IActionResult Calculator(Operator? op, double? x, double? y)
     {
-        return Result();
+
+
+
+        if (op == Operator.Sin)
+        {
+            if (x is null)
+            {
+                ViewBag.ErrorMessage = "Niepoprawny format parametru x.";
+                return View("CalculatorError");
+            }
+
+            ViewBag.Result = Math.Sin(x.Value);
+            return View();
+        }
+
+
+
+        if (x is null || y is null)
+        {
+            ViewBag.ErrorMessage = "Niepoprawny format parametru x lub y";
+            return View("CalculatorError");
+        }
+
+        if (op is null)
+        {
+            ViewBag.ErrorMessage = "Nieznany operator!";
+            return View("CalculatorError");
+        }
+
+        switch (op)
+        {
+            case Operator.Add:
+                ViewBag.Result = x + y;
+                break;
+            case Operator.Div:
+                ViewBag.Result = x - y;
+                break;
+            case Operator.Mul:
+                ViewBag.Result = x * y;
+                break;
+            case Operator.Sub:
+                ViewBag.Result = x / y;
+                break;
+            case Operator.Pow:
+                ViewBag.Result = Math.Pow(x.Value, y.Value);
+                break;
+            case Operator.Sin:
+
+                ViewBag.Result = Math.Sin(x.Value);
+                break;
+        }
+
+        return View();
+    }
+    public IActionResult Form()
+    {
+        return View();
     }
 }
+
+public enum Operator
+    {
+        Add, Sub, Mul, Div, Pow, Sin
+    }
+    
