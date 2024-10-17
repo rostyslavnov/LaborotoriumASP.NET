a1108d073409749a8bd14d035790432b348a9cef
rostyslave <rostyslavenovytskyi@microsoft.wsei.edu.pl>
2024-10-17 23:20:19 +0200

    Gotowe zadanie #2.

---
+++ WebApp/Controllers/BirthController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApp.Models;
+
+namespace WebApp.Controllers
+{
+    public class BirthController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View(); 
+        }
+
+        [HttpPost]
+        public IActionResult Submit(Birth model)
+        {
+            if (model.IsValid())
+            {
+                int age = model.CalculateAge();
+                ViewBag.Message = $"Cześć {model.Name}, masz {age} lat(a).";
+            }
+            else
+            {
+                ViewBag.Message = "Nieprawidłowe dane.";
+            }
+
+            return View("Index");
+        }
+    }
+}
---
+++ WebApp/Models/Birth.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace WebApp.Models
+{
+    public class Birth
+    {
+        public string Name { get; set; }
+        public DateTime BirthDate { get; set; }
+        
+        public bool IsValid()
+        {
+            return !string.IsNullOrEmpty(Name) && BirthDate < DateTime.Now;
+        }
+
+        public int CalculateAge()
+        {
+            var today = DateTime.Today;
+            var age = today.Year - BirthDate.Year;
+
+            if (BirthDate > today.AddYears(-age))
+            {
+                age--;
+            }
+
+            return age;
+        }
+    }
+}
